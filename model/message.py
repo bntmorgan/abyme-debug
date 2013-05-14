dc0b0c79b519a592c1793a91ecfcef7e66f1ce7e
garage <garage@noob.(none)>
2013-05-14 10:12:38 +0200

    Formattage mieux

--- model/message.py
+++ model/message.py
@@ -72,13 +72,13 @@ class MessageIn(Message):
     b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (ord(a[0]), ord(a[1]), ord(a[2]), ord(a[3]), ord(a[4]), ord(a[5]))
     return b
   def format(self):
-    l = "%04d B" % (self.frame.headerLength)
+    l = "%04d B" % (len(self.frame.payload))
     # new length addrSource addrDest type
     return "%04d %d %s %s %s" % (self.number, self.core, l, MessageIn.ethAddr(self.frame.macSource), MessageIn.ethAddr(self.frame.macDest))
   def formatFull(self):
-    l = "%04d B" % (self.frame.headerLength)
+    l = "%04d B" % (len(self.frame.payload))
     # new length addrSource addrDest type
-    return "number : %04d\ncore : %d\nheader length : %s\nsrc address : %s\ndest address : %s" % (self.number, self.core, l, MessageIn.ethAddr(self.frame.macSource), MessageIn.ethAddr(self.frame.macDest))
+    return "number : %04d\ncore : %d\nlength : %s\nsrc address : %s\ndest address : %s" % (self.number, self.core, l, MessageIn.ethAddr(self.frame.macSource), MessageIn.ethAddr(self.frame.macDest))
 
 class MessageVMExit(MessageIn):
   def __init__(self):
@@ -96,39 +96,34 @@ class MessageVMExit(MessageIn):
   def formatFull(self):
     return MessageIn.formatFull(self) + "\nexit reason : 0x%x (%d)" % (self.exitReason, self.exitReason & 0xffff)
 
-FILTER=''.join([(len(repr(chr(x)))==3) and chr(x) or '.' for x in range(256)])
+FILTER=''.join([(len(repr(chr(x))) == 3) and chr(x) or '.' for x in range(256)])
 class MessageMemoryData(MessageIn):
   def __init__(self):
     MessageIn.__init__(self)
     self.messageType = Message.MemoryData
+    self.address = 0
     self.length = 0
   def format(self):
     return "%s MemoryData" % (MessageIn.format(self))
   def unPack(self):
     MessageIn.unPack(self)
-    t = unpack('q', self.frame.payload[2:10])
-    self.length = t[0]
-    self.data = self.frame.payload[10:10 + self.length] 
+    t = unpack('qq', self.frame.payload[2:18])
+    self.address = t[0]
+    self.length = t[1]
+    self.data = self.frame.payload[18:18 + self.length] 
   def pack(self):
-    return MessageIn.pack(self) + pack('q', self.length)
+    return MessageIn.pack(self) + pack('qq', self.address, self.length)
   def formatFull(self):
-    return MessageIn.formatFull(self) + "\nlength : 0x%x\n%s" % (self.length, self.dumpAll())
-  def dumpAll(self):
-    result = ''
-    cycles = math.ceil(len(self.data) / 16)
-    s = self.data
-    for i in range(cycles):
-      self.dump(s, 16)
-      s = s[16:]
-    return result
-  def dump(self, src, length):
-    N=0; result=''
+    return MessageIn.formatFull(self) + "\naddress : 0x%x\nlength : 0x%x\n%s" % (self.address, self.length, self.dump(16, self.address))
+  def dump(self, length, n):
+    src = self.data
+    result=''
     while src:
-     s,src = src[:length],src[length:]
-     hexa = ' '.join(["%02X"%ord(x) for x in s])
+     s,src = src[:length], src[length:]
+     hexa = ' '.join(["%02X" % ord(x) for x in s])
      s = s.translate(FILTER)
-     result += "%04X   %-*s   %s\n" % (N, length*3, hexa, s)
-     N+=length
+     result += "%016X   %-*s   %s\n" % (n, length * 3, hexa, s)
+     n += length
     return result
 
 '''
